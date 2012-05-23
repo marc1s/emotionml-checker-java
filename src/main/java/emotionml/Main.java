84df79e14c4c06eaae4cdd129cf8b4dce8653856
Marc Schröder <marc.schroeder@dfki.de>
2012-05-23 14:00:58 +0200

    Usage if called without command line options

--- src/main/java/emotionml/Main.java
+++ src/main/java/emotionml/Main.java
@@ -7,6 +7,10 @@ import emotionml.exceptions.ConfigurationException;
 public class Main {
 
 	public static void main(String[] args) throws ConfigurationException {
+		if (args.length == 0) {
+			usage();
+			System.exit(0);
+		}
 		Checker checker = new Checker();
 		for (String filename : args) {
 			System.err.print(filename+"... ");
@@ -18,5 +22,13 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private static void usage() {
+		System.out.println("Usage:");
+		System.out.println();
+		System.out.println("java -jar emotionml-checker-java.jar file.emotionml [more emotionml files]");
+		System.out.println();
+		System.out.println("where `file.emotionml` is an XML file containing the EmotionML document to be validated.");
 	}
 }
